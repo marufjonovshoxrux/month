2060271b06f6d88bc19b0f8f663581791f5423ba
Shoxrux Marufjonov <shokha0502@gmail.com>
2024-04-07 12:09:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,97 @@
+let month = prompt("Какой месяц")
+if (month ==  1) {
+    alert("Январь")
+
+} else if (month == 2) {
+    alert("Февраль")
+}
+else if (month == 3) {
+    alert("Март")
+}
+else if (month == 4) {
+    alert("Апрель")
+}
+else if (month == 5) {
+    alert("Май")
+}
+else if (month == 6) {
+    alert("Июнь")
+}
+else if (month == 7) {
+    alert("Июль")
+}
+else if (month == 8) {
+    alert("Август")
+}
+else if (month == 9) {
+    alert("Сентябрь")
+}
+else if (month == 10) {
+    alert("Октябрь")
+}
+else if (month == 11) {
+    alert("Ноябрь")
+}
+else if (month == 12) {
+    alert("Декабрь")
+}
+ else {
+    alert("Такого месяца нету")
+}
+
+// let month = +prompt("Какой месяц")
+
+// switch (month) {
+//     case 1:
+//         alert("Январь")
+        
+//         break;
+//     case 2:
+//         alert("Февраль")
+        
+//         break;
+//     case 3:
+//         alert("Март")
+        
+//         break;
+//     case 4:
+//         alert("Апрель")
+        
+//         break;
+//     case 5:
+//         alert("Май")
+        
+//         break;    
+//     case 6:
+//         alert("Июнь")
+        
+//         break;
+//     case 7:
+//         alert("Июль")
+        
+//         break;
+//     case 8:
+//         alert("Август")
+        
+//         break;
+//     case 9:
+//         alert("Сентябрь")
+        
+//         break;
+//     case 10:
+//         alert("Октябрь")
+        
+//         break;
+//     case 11:
+//         alert("Ноябрь")
+        
+//         break;
+//     case 12:
+//         alert("Декабрь")
+        
+//         break;
+   
+//     default:
+//         alert("Такого месяца нету")
+//         break;
+// }
